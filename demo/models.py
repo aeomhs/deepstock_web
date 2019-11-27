04cb56de9d1e8d218647e21394085c7fc16793c4
Hyunsik AEOM <t_aeom@naver.com>
2019-11-27 22:35:56 +0900

    company, predicted price model init

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(verbose_name='종목명',max_length=30)
+    code = models.CharField(verbose_name='종목 코드',max_length=6)
+    price = models.IntegerField(verbose_name='전일 종가')
+    biz_type = models.CharField(verbose_name='업종',max_length=20)
+    market_type = models.CharField(verbose_name='소속',max_length=2)
+
+    class Meta:
+        verbose_name='종목'
+        verbose_name_plural='주식 종목'
+        ordering=['-price']
+
+class PredictedPrice(models.Model):
+    company = models.OneToOneField(
+        Company,
+        on_delete=models.CASCADE,
+        verbose_name='종목명',
+    )
+    now = models.IntegerField(verbose_name='전일 종가')
+    tomorrow = models.IntegerField(verbose_name='명일 예측 종가')
+    after_a_month = models.IntegerField(verbose_name='명월 예측 종가')
+    
+    class Meta:
+        verbose_name='종목 주가 예측'
+        verbose_name_plural='예측 주가'
+        ordering=['-now']
