7a3cb63436e4f971a5e7eabec3b92ba7fc491383
Hyunsik AEOM <t_aeom@naver.com>
2019-11-28 17:53:23 +0900

    remove predictPrice add Price, fixed Model

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, PredictedPrice
+from .models import Company, Price
 # Register your models here.
 admin.site.register(Company)
-admin.site.register(PredictedPrice)
+admin.site.register(Price)
--- demo/models.py
+++ demo/models.py
@@ -1,28 +1,66 @@
 from django.db import models
 
+class KospiCompanyManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(market_type='KP')
+
+class KosdaqCompanyMananger(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(market_type='KD')
+
 class Company(models.Model):
+    # CHOICES
+    kospi_type = 'KP'
+    kosdaq_type = 'KD'
+    MARKET_TYPE_CHOICES = (
+        (kospi_type, '코스피'),
+        (kosdaq_type, '코스닥'),
+    )
+
     name = models.CharField(verbose_name='종목명',max_length=30)
     code = models.CharField(verbose_name='종목 코드',max_length=6)
-    price = models.IntegerField(verbose_name='전일 종가')
-    biz_type = models.CharField(verbose_name='업종',max_length=20)
-    market_type = models.CharField(verbose_name='소속',max_length=2)
+    market_type = models.CharField(verbose_name='소속',max_length=2,choices=MARKET_TYPE_CHOICES)  
+
+    objects = models.Manager()
+    kospi_companies = KospiCompanyManager()
+    kosdaq_companies = KosdaqCompanyMananger()
 
     class Meta:
         verbose_name='종목'
         verbose_name_plural='주식 종목'
-        ordering=['-price']
+        ordering=['name']
 
-class PredictedPrice(models.Model):
-    company = models.OneToOneField(
+    def __str__(self):
+        return self.name
+
+class CompanyPriceManager(models.Manager):
+    def get_queryset(self, _company):
+        return super().get_queryset().filter(company=_company)
+
+class Price(models.Model):
+    company = models.ForeignKey(
         Company,
         on_delete=models.CASCADE,
         verbose_name='종목명',
     )
-    now = models.IntegerField(verbose_name='전일 종가')
-    tomorrow = models.IntegerField(verbose_name='명일 예측 종가')
-    after_a_month = models.IntegerField(verbose_name='명월 예측 종가')
+
+    date = models.DateField(
+        verbose_name='날짜',
+    )
+
+    price = models.IntegerField(
+        verbose_name='시가',
+    )
     
+    objects = models.Manager()
+    company_price = CompanyPriceManager()
+
     class Meta:
-        verbose_name='종목 주가 예측'
-        verbose_name_plural='예측 주가'
-        ordering=['-now']
+        verbose_name='주가'
+        verbose_name_plural='날짜별 주가'
+        # 날짜별 각 종목의 시가는 단 하나의 칼럼으로 이루어진다.
+        unique_together=(('company', 'date'),)
+        ordering=['company']
+        
+    def __str__(self):
+        return "["+self.company.__str__()+"]"+self.date.strftime('%Y/%m/%d')
