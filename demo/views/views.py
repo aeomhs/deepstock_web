869fe5d2cd70b1fd29e1983bcb4682ab3bc3c6fa
KimReady <kjb4380@gmail.com>
2019-12-13 22:30:13 +0900

    Add a table for list of the stock

--- demo/views/views.py
+++ demo/views/views.py
@@ -18,13 +18,14 @@ def stock_list(request):
     # kosdaq_list = Company.kosdaq_companies.all()
     kospi_list = CompanyPrice.objects.get_kospi_stock_list()
     kosdaq_list = CompanyPrice.objects.get_kosdaq_stock_list()
-        
+
     stock_list = {
         'kospi_list' : kospi_list,
         'kosdaq_list' : kosdaq_list,
     }
 
-    return render(request, 'demo/stock_list.html', {'stock_list':stock_list})
+    return render(request, 'demo/table.html')
+    # return render(request, 'demo/stock_list.html', {'stock_list':stock_list})
 
 # TODO 종목 분석 페이지 구현
 # 요소 : 그래프, 설명, 관련 뉴스
